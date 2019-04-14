e67e9f2e853c26bdf34fee3873bfe1d52c2f2688
damien <damien.fauveau@outlook.com>
2019-04-14 15:01:07 +0200

    Added choice modal

--- app/script.js
+++ app/script.js
@@ -1,10 +1,22 @@
+//document.addEventListener('contextmenu', event => event.preventDefault());
+
 document.addEventListener('DOMContentLoaded', function(event) {
 
 	document.getElementById('gameContainer').onclick = function() {
-	  	xpos = window.event.clientX;
-    	ypos = window.event.clientY;
-    	document.getElementById('waiter').style.top = ypos - 30 + 'px';
-    	document.getElementById('waiter').style.left = xpos - 20 + 'px';
+		xpos = window.event.clientX;
+		ypos = window.event.clientY;
+		document.getElementById('waiter').style.top = ypos - 30 + 'px';
+		document.getElementById('waiter').style.left = xpos - 20 + 'px';
 	};
+
+	document.getElementById("myBtn").onclick = function() {
+		document.getElementById('modalChoice').style.display = "block";
+	}
+	
+	Array.from(document.getElementsByClassName("close")).forEach(function(element) {
+      	element.addEventListener('click', function() {
+			document.getElementById('modalChoice').style.display = "none";
+		});
+    });
 
 });
