e61e2a82eefd86d1424f357306466e0a543a6628
damien <damien.fauveau@outlook.com>
2019-04-16 22:04:33 +0200

    Added validation cocktails

--- app/script.js
+++ app/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 		xpos = window.event.clientX;
 		ypos = window.event.clientY;
 		document.getElementById('waiter').style.top = ypos - 30 + 'px';
-		document.getElementById('waiter').style.left = xpos - 20 + 'px';
+		document.getElementById('waiter').style.left = xpos - 25 + 'px';
 	};
 
 	document.getElementById("myBtn").onclick = function() {
@@ -24,7 +24,26 @@ document.addEventListener('DOMContentLoaded', function(event) {
 	};
 
     document.getElementById('buttonCocktail').onclick = function() {
+    	var chosenIng = [];
+    	var correctIng = cocktailMojito();
+		
+		Array.prototype.forEach.call(document.getElementsByClassName("ingredient"), function(element, index) {
+			if(element.checked)
+		    	chosenIng.push(element['value']);
+		});
+		
+		var nbPoints = document.getElementById('numberPoints').innerHTML;
+		if(JSON.stringify(chosenIng) === JSON.stringify(correctIng)) {
+			document.getElementById('numberPoints').innerHTML = parseInt(nbPoints, 10) + 1;
+		} else {
+			document.getElementById('numberPoints').innerHTML = parseInt(nbPoints, 10) - 1;
+		}
+		
 		document.getElementById('modalCocktail').style.display = "none";
 	};
 
 });
+
+function cocktailMojito() {
+	return ["mint","sugar","rhum"];
+}
